2bd0c40856208d5ea297c5bee5fd694e2af4c04b
me-lucas-al <me.lucasalmeida@gmail.com>
2026-05-11 17:58:08 -0300

    feat(auth): create user domain model and repository

---
+++ api/src/main/java/com/locuspark/api/enums/UserRole.java
@@ -0,0 +1,11 @@
+package com.locuspark.api.enums;
+
+public enum UserRole {
+    ADMIN("ADMIN"),
+    USER("USER");
+
+    private final String role;
+
+    UserRole(String role) { this.role = role; }
+    public String getRole() { return role; }
+}
